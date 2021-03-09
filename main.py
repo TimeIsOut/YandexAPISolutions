bf42f17719a2b65b06e71a4483188411d89ed784
TimeIsOut <y0u-g0t-hack3d@yandex.ru>
2021-03-09 09:55:41 +0300

    Настроена политика для №3.

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ class MapVision(QtWidgets.QMainWindow, Ui_Form):
         if result:
             try:
                 mkdir("data/")
-            except Exception:
+            except Exception as e:
                 pass
             with open("data/map.png", "wb") as file:
                 file.write(result.content)
             self.map.setPixmap(QtGui.QPixmap("data/map.png"))
+            for childWidget in self.findChildren(QtWidgets.QWidget):
+                childWidget.setFocusPolicy(QtCore.Qt.ClickFocus)
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_PageUp:
@@ -36,6 +38,30 @@ class MapVision(QtWidgets.QMainWindow, Ui_Form):
             if scaling > 0:
                 self.scale.setText(str(scaling - 1))
             self.showing()
+        elif event.key() == QtCore.Qt.Key_Up:  # нажатие на стрелку вверх
+            lon = float(self.lon.text())  # текущая широта
+            scaling = int(self.scale.text())  # масштаб карты
+            self.lon.setText(str(lon + ((18 - scaling) / scaling ** 2.5)))
+            # изменение по формуле: предидущее значение + ((18 - масштаб) / масштаб ** 2.5)
+            self.showing()  # отрисовка карты
+        elif event.key() == QtCore.Qt.Key_Down:
+            lon = float(self.lon.text())  # текущая широта
+            scaling = int(self.scale.text())  # масштаб карты
+            self.lon.setText(str(lon - ((18 - scaling) / scaling ** 2.5)))
+            # изменение по формуле: предидущее значение - ((18 - масштаб) / масштаб ** 2.5)
+            self.showing()  # отрисовка карты
+        elif event.key() == QtCore.Qt.Key_Left:
+            lat = float(self.lon.text())  # текущая долгота
+            scaling = int(self.scale.text())  # масштаб карты
+            self.lat.setText(str(lat - ((18 - scaling) / scaling ** 2.5)))
+            # изменение по формуле: предыдущее значение + ((18 - масштаб) / масштаб ** 2.5)
+            self.showing()  # отрисовка карты
+        elif event.key() == QtCore.Qt.Key_Right:
+            lat = float(self.lon.text())  # текущая долгота
+            scaling = int(self.scale.text())  # масштаб карты
+            self.lat.setText(str(lat + ((18 - scaling) / scaling ** 2.5)))
+            # изменение по формуле: предидущее значение - ((18 - масштаб) / масштаб ** 2.5)
+            self.showing()  # отрисовка карты
 
 
 if __name__ == "__main__":
